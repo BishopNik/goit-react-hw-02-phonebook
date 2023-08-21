15b1b317a64e0c5f4d3797c1fa4fdf3ee1eb3d1e
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-08-21 22:02:26 +0200

    structure full

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,141 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+/** @format */
+
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import './style.css';
+
+class App extends Component {
+	state = {
+		contacts: [
+			{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+			{ id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+			{ id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+			{ id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+		],
+		name: '',
+		number: '',
+		filter: '',
+	};
+
+	handlerOnChange = ({ target }) => {
+		this.setState({
+			[target.name]: target.value,
+		});
+	};
+
+	handlerOnSumbit = e => {
+		this.handleClick(e);
+	};
+
+	handlerOnFitred = ({ target }) => {
+		this.setState({
+			[target.name]: target.value,
+		});
+	};
+
+	handleAddContact = e => {
+		e.preventDefault();
+		this.setState(prevState => ({
+			contacts: [
+				...prevState.contacts,
+				{
+					id: nanoid(),
+					name: this.state.name,
+					number: this.state.number,
+				},
+			],
+			name: '',
+			number: '',
+		}));
+	};
+
+	handleDelClick = e => {
+		this.setState(prevState => {
+			const updatedContacts = prevState.contacts.filter(
+				contact => contact.id !== e.target.id
+			);
+			return { contacts: updatedContacts };
+		});
+	};
+
+	handleClick = ({ target }) => {
+		target.style.scale = '0.9';
+		setTimeout(() => (target.style.scale = '1'), 80);
+	};
+
+	render() {
+		return (
+			<div className='container'>
+				<h1 className='title-name'>Phonebook</h1>
+				<form className='form-contact' onSubmit={this.handleAddContact}>
+					<label className='label'>
+						Name
+						<input
+							className='input-field'
+							value={this.state.name}
+							type='text'
+							name='name'
+							pattern="^[a-zA-Zа-яА-Я]+([' -]?[a-zA-Zа-яА-Я ])*$"
+							title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+							required
+							autoComplete='off'
+							onChange={this.handlerOnChange}
+						/>
+					</label>
+					<label className='label'>
+						Number
+						<input
+							className='input-field'
+							value={this.state.number}
+							type='tel'
+							name='number'
+							pattern='\+?\d{1,4}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}'
+							title='Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+							required
+							autoComplete='off'
+							onChange={this.handlerOnChange}
+						/>
+					</label>
+					<button className='add-contact button' type='submit' onClick={this.handleClick}>
+						Add contact
+					</button>
+				</form>
+				<h2 className='title-name'>Contacts</h2>
+				<label className='filter-field'>
+					Find contacts by name
+					<input
+						className='input-field filter'
+						value={this.state.filter}
+						type='text'
+						name='filter'
+						autoComplete='off'
+						onChange={this.handlerOnFitred}
+					/>
+				</label>
+				{this.state.contacts
+					.filter(contact => {
+						const searchName = contact.name.toLowerCase();
+						const filterName = this.state.filter.toLowerCase();
+						return searchName.includes(filterName);
+					})
+					.map(({ id, name, number }) => (
+						<div className='contact-containet' key={id}>
+							<p className='contact'>
+								{name} {number}
+							</p>
+							<button
+								id={id}
+								className='del-button button'
+								type='submit'
+								onClick={this.handleDelClick}
+							>
+								Delete
+							</button>
+						</div>
+					))}
+			</div>
+		);
+	}
+}
+
+export default App;
